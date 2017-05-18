e80ff3ff6a17e44fb001c8052c7670b8f8b11bd9
김영재 <yjinzum@zuminternet.com>
2017-05-18 13:18:55 +0900

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# spring-boot-social-comment
+
+### Evironment
+- Java8
+- Spring Boot 1.5.2
+- Spring Security oauth2
+- Spring Data Jpa
+- JPA, Querydsl
+- lombok
+- logback
+- Gradle 3.5
---
+++ social-comment/src/main/java/com/social/CommentApplication.java
@@ -0,0 +1,12 @@
+package com.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentApplication.class, args);
+	}
+}
