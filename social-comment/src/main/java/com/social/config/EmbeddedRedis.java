8ebc2e4d42d502ede44f184890d3f36fbf234eee
김영재 <yjinzum@zuminternet.com>
2017-06-07 09:51:20 +0900

    add embbeded redis

---
+++ social-comment/src/main/java/com/social/config/EmbeddedRedis.java
@@ -0,0 +1,30 @@
+package com.social.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import redis.embedded.RedisServer;
+
+/**
+ * Created by KimYJ on 2017-06-05.
+ */
+@Component
+public class EmbeddedRedis {
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer();
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+}
